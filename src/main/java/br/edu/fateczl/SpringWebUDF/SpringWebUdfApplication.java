4bfc08ecc420d94a07a218a299eefdc14f69d8bd
luiza <luiza@DESKTOP-82K6BJH>
2024-04-08 17:11:56 -0300

    configuração inicial

---
+++ src/main/java/br/edu/fateczl/SpringWebUDF/SpringWebUdfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.SpringWebUDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebUdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebUdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fateczl/SpringWebUDF/SpringWebUdfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.SpringWebUDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebUdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
